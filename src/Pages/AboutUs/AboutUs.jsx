42ae881602291af81e87ff3830e1db4f65d5c61b
Fahima Mohamadi <69745200+Fahimamohamadi@users.noreply.github.com>
2024-02-19 12:06:50 +1100

    WIP

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -28,6 +28,25 @@ function AboutUS() {
                     Then, you will describe the different visual states for each of your components. Finally, you will connect
                     your components together so that the data flows through them. In this tutorial, we’ll guide you through the
                     thought process of building a searchable product data table with React.
+                    </p>
+                    <p>
+                    React can change how you think about the designs you look at and the apps you build.
+                    When you build a user interface with React, you will first break it apart into pieces called components.
+                    Then, you will describe the different visual states for each of your components. Finally, you will connect
+                    your components together so that the data flows through them. In this tutorial, we’ll guide you through the
+                    thought process of building a searchable product data table with React.
+                    
+                    React can change how you think about the designs you look at and the apps you build.
+                    When you build a user interface with React, you will first break it apart into pieces called components.
+                    Then, you will describe the different visual states for each of your components. Finally, you will connect
+                    your components together so that the data flows through them. In this tutorial, we’ll guide you through the
+                    thought process of building a searchable product data table with React.
+                    
+                    React can change how you think about the designs you look at and the apps you build.
+                    When you build a user interface with React, you will first break it apart into pieces called components.
+                    Then, you will describe the different visual states for each of your components. Finally, you will connect
+                    your components together so that the data flows through them. In this tutorial, we’ll guide you through the
+                    thought process of building a searchable product data table with React.
                  </p>
                 <img src={"https://th.bing.com/th/id/OIP.4roXKw8CY20WO1KGiw7HjwHaE8?rs=1&pid=ImgDetMain"} alt="eagle" />
             </div>
